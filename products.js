e08793cad11275bf57c506a1ddf56f1462f26ac5
sblinov <sematik.mail@gmail.com>
2024-03-01 14:08:15 +0300

    Delete products.js

--- products.js
+++
@@ -1,5 +0,0 @@
-const products = {
-    "coffee": ["Cappuccino", "Latte", "Espresso", "Americano"],
-    "tea": ["Black tea", "Green tea", "White tea", "Oolong", "Pu-erh tea"],
-    "bakery": ["Croissant", "Cupcake", "Cookie", "Cake", "Donut"]
-}
